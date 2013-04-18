42520ec19697a70ec1346dcbe210a52e6b73e182
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2013-04-17 21:19:42 -1000

    Whoops, remove previous changes.
    Previous commits made actual development changes to a sample child theme.  They have been removed and put in a for-real child theme.

---
+++ wp-content/themes/HULAchildKCM/functions.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * To include custom PHP functions in the child theme, include them below.
+ */
+?>
+<?php
+/* Example:
+ * To add a favicon to the head of Wordpress pages, you could do this:
+ *  function favicon_link() {
+ *    echo '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />' . "\n";
+ *  }
+ *  add_action('wp_head', 'favicon_link');
+ */
+?>
---
+++ wp-content/themes/HULAchildKCM/sample-template.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Sample-Template
+ * 
+ * @file           sample-template.php
+ * @package        Sample Child
+ * @author         K. Clay McKell 
+ */
+?>
+
+<?php
+/*
+ * Customize your page template by changing the filename, Template Name (above), and code below.
+ * For help with building templates, check out:
+ * http://codex.wordpress.org/Templates - Templates home page.
+ * http://codex.wordpress.org/Pages#Page_Templates - Help for templates for Pages specifically.
+ * http://codex.wordpress.org/Stepping_Into_Templates - More detailed help on template structure.
+ */
+?>
+
+<?php
+get_template_part( 'full-width', 'page' );
+?>
