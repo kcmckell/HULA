7282c0ced72b30b885286a4b5e27386dbd89284a
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2013-04-25 14:39:43 -1000

    Edit email cross-posting function.
    Now the emailing cross-post function is called on any post publication
    action.  Also, it is only performed on posts that have the "HULAmail"
    tag added to them.

--- wp-content/themes/HULAchildKCM/functions.php
+++ wp-content/themes/HULAchildKCM/functions.php
@@ -24,13 +24,41 @@
  *  add_action('wp_head', 'favicon_link');
  */
 
-add_action('publish_post','MyFunctionOnPublish');
+add_action('new_to_publish','MyFunctionOnPublish');
+add_action('pending_to_publish', 'MyFunctionOnPublish');
+add_action('draft_to_publish','MyFunctionOnPublish');
+add_action('future_to_publish','MyFunctionOnPublish');
 
 function MyFunctionOnPublish($post_id) {
   // Attempt to forward posts to the HULA email group.
   $post = get_post($post_id);
-  $from_email = get_the_author_meta('user_email', $post->post_author);
-  $headers = array('From: ' . $from_email);
-  wp_mail('hawaii-ultimate-league-association@googlegroups.com', $post->post_title, $post->post_content, $headers);
+  if (has_tag("hulamail", $post)) {
+    $from_name = get_the_author_meta('display_name', $post->post_author);
+    $from_email = get_the_author_meta('user_email', $post->post_author);
+    $headers = array('From: ' . $from_name . ' <' . $from_email . '>');
+//    $msg = '<html>
+//      <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
+//      <table width="100%" bgcolor="#499cff" cellpadding="10" cellspacing="0">
+//        <tr valign="top" align="center">
+//          <td>
+//            <table width="500" cellspacing="0" cellpadding="0" bgcolor="#ffffff">
+//            <tr>
+//            <td style="padding: 15px">'
+//              . $post->post_content . 
+//            '</td>
+//            </tr>
+//            </table>
+//          </td>
+//        </tr>
+//      </table>
+//      </body>
+//      </html>';
+      $msg = $post->post_content;
+//    wp_mail('hawaii-ultimate-league-association@googlegroups.com', $post->post_title, $post->post_content, $headers);
+// Debug mode:    
+    wp_mail('testingwordpressemail@googlegroups.com', $post->post_title, $msg, $headers);
+  } else {
+    // Nothing to see here.
+  };
 };
 ?>
