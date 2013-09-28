9c9d13595c6cb2bc01144ef0e872195df014db04
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2013-09-27 18:07:37 -1000

    Reformatting current LeagueVine games.
    Larger text size for current games.
    Also color-code winners and losers.
    TODO: Perhaps differentiate between big wins and small leads by shading
    the colors on an RGB scale.

--- wp-content/themes/HULAchildKCM/js/LV.js
+++ wp-content/themes/HULAchildKCM/js/LV.js
@@ -4,23 +4,52 @@
  */
 
 //alert(LV_api_url);
+
+//var demo = {"meta": {"limit": 20, "next": null, "offset": 0, "previous": null, "total_count": 3}, "objects": [{"bracket": null, "bracket_id": null, "game_site": {"event_site": {"description": "", "id": 20424, "image": "", "image_400": "", "name": "Kapiolani Park", "resource_uri": "https://api.leaguevine.com/v1/event_sites/20424/", "time_created": "2013-09-27T21:53:31.629643+00:00", "time_last_updated": "2013-09-27T21:53:31.629703+00:00"}, "event_site_id": 20424, "id": 21712, "name": "Field 3", "resource_uri": "https://api.leaguevine.com/v1/game_sites/21712/", "time_created": "2013-09-27T21:53:32.197276+00:00", "time_last_updated": "2013-09-27T21:53:32.197306+00:00"}, "game_site_id": 21712, "id": 126373, "leaguevine_url": "https://www.leaguevine.com/games/126373/", "next_game_for_loser": null, "next_game_for_loser_id": null, "next_game_for_winner": null, "next_game_for_winner_id": null, "next_team_for_loser": 1, "next_team_for_winner": 1, "number_of_sets": null, "pool": null, "pool_id": null, "pool_round": null, "pool_round_id": null, "resource_uri": "https://api.leaguevine.com/v1/games/126373/", "season": {"end_date": null, "id": 20368, "league": {"id": 20367, "leaguevine_url": "https://www.leaguevine.com/leagues/20367/hula/", "name": "HULA", "resource_uri": "https://api.leaguevine.com/v1/leagues/20367/"}, "league_id": 20367, "leaguevine_url": "https://www.leaguevine.com/seasons/20368/hula-fall-2013/", "name": "Fall 2013", "resource_uri": "https://api.leaguevine.com/v1/seasons/20368/", "start_date": null, "time_created": "2013-09-07T00:34:56.568775+00:00", "time_last_updated": "2013-09-07T00:34:56.568815+00:00"}, "season_id": 20368, "start_time": "2013-10-05T10:00:00-10:00", "swiss_round": null, "swiss_round_id": null, "team_1": {"id": 24367, "leaguevine_url": "https://www.leaguevine.com/teams/24367/black-panthers/", "name": "Black Panthers", "resource_uri": "https://api.leaguevine.com/v1/teams/24367/", "short_name": "Black"}, "team_1_id": 24367, "team_1_score": 0, "team_2": {"id": 24368, "leaguevine_url": "https://www.leaguevine.com/teams/24368/orange-crush/", "name": "Orange Crush", "resource_uri": "https://api.leaguevine.com/v1/teams/24368/", "short_name": "Orange"}, "team_2_id": 24368, "team_2_score": 0, "time_created": "2013-09-27T21:40:50.483431+00:00", "time_last_updated": "2013-09-27T21:54:03.752522+00:00", "timezone": "US/Hawaii", "tournament": null, "tournament_id": null, "winner": null, "winner_id": null}, {"bracket": null, "bracket_id": null, "game_site": null, "game_site_id": null, "id": 126372, "leaguevine_url": "https://www.leaguevine.com/games/126372/", "next_game_for_loser": null, "next_game_for_loser_id": null, "next_game_for_winner": null, "next_game_for_winner_id": null, "next_team_for_loser": 1, "next_team_for_winner": 1, "number_of_sets": null, "pool": null, "pool_id": null, "pool_round": null, "pool_round_id": null, "resource_uri": "https://api.leaguevine.com/v1/games/126372/", "season": {"end_date": null, "id": 20368, "league": {"id": 20367, "leaguevine_url": "https://www.leaguevine.com/leagues/20367/hula/", "name": "HULA", "resource_uri": "https://api.leaguevine.com/v1/leagues/20367/"}, "league_id": 20367, "leaguevine_url": "https://www.leaguevine.com/seasons/20368/hula-fall-2013/", "name": "Fall 2013", "resource_uri": "https://api.leaguevine.com/v1/seasons/20368/", "start_date": null, "time_created": "2013-09-07T00:34:56.568775+00:00", "time_last_updated": "2013-09-07T00:34:56.568815+00:00"}, "season_id": 20368, "start_time": "2013-10-12T12:00:00-10:00", "swiss_round": null, "swiss_round_id": null, "team_1": {"id": 24367, "leaguevine_url": "https://www.leaguevine.com/teams/24367/black-panthers/", "name": "Black Panthers", "resource_uri": "https://api.leaguevine.com/v1/teams/24367/", "short_name": "Black"}, "team_1_id": 24367, "team_1_score": -1, "team_2": {"id": 24368, "leaguevine_url": "https://www.leaguevine.com/teams/24368/orange-crush/", "name": "Orange Crush", "resource_uri": "https://api.leaguevine.com/v1/teams/24368/", "short_name": "Orange"}, "team_2_id": 24368, "team_2_score": 0, "time_created": "2013-09-27T20:15:02.077482+00:00", "time_last_updated": "2013-09-27T20:15:02.077532+00:00", "timezone": "US/Hawaii", "tournament": null, "tournament_id": null, "winner": null, "winner_id": null}, {"bracket": null, "bracket_id": null, "game_site": null, "game_site_id": null, "id": 124405, "leaguevine_url": "https://www.leaguevine.com/games/124405/", "next_game_for_loser": null, "next_game_for_loser_id": null, "next_game_for_winner": null, "next_game_for_winner_id": null, "next_team_for_loser": 1, "next_team_for_winner": 1, "number_of_sets": null, "pool": null, "pool_id": null, "pool_round": null, "pool_round_id": null, "resource_uri": "https://api.leaguevine.com/v1/games/124405/", "season": {"end_date": null, "id": 20368, "league": {"id": 20367, "leaguevine_url": "https://www.leaguevine.com/leagues/20367/hula/", "name": "HULA", "resource_uri": "https://api.leaguevine.com/v1/leagues/20367/"}, "league_id": 20367, "leaguevine_url": "https://www.leaguevine.com/seasons/20368/hula-fall-2013/", "name": "Fall 2013", "resource_uri": "https://api.leaguevine.com/v1/seasons/20368/", "start_date": null, "time_created": "2013-09-07T00:34:56.568775+00:00", "time_last_updated": "2013-09-07T00:34:56.568815+00:00"}, "season_id": 20368, "start_time": "2013-10-05T10:00:00-10:00", "swiss_round": null, "swiss_round_id": null, "team_1": {"id": 24318, "leaguevine_url": "https://www.leaguevine.com/teams/24318/green-goblins/", "name": "Green Goblins", "resource_uri": "https://api.leaguevine.com/v1/teams/24318/", "short_name": "Green"}, "team_1_id": 24318, "team_1_score": 1, "team_2": {"id": 24317, "leaguevine_url": "https://www.leaguevine.com/teams/24317/red-raiders/", "name": "Red Raiders", "resource_uri": "https://api.leaguevine.com/v1/teams/24317/", "short_name": "Red"}, "team_2_id": 24317, "team_2_score": 0, "time_created": "2013-09-10T22:51:34.109794+00:00", "time_last_updated": "2013-09-10T22:51:34.109912+00:00", "timezone": "US/Hawaii", "tournament": null, "tournament_id": null, "winner": null, "winner_id": null}]};
+
 jQuery(document).ready(function() {
   var $LV = jQuery('div.LV');
   var now = new Date();
   var recent = new Date(now - 15*60*1000);
+  // ***********************
   // Get games currently on.
+  // ***********************
   jQuery.getJSON(LV_api_url+'games/?season_id=20368'+
     '&starts_after='+recent.toISOString()+
     '&starts_before='+now.toISOString()+
     '&access_token='+LV_access_token,
     function(json){
-      renderGames(json,1,'#LV-current');
+  // DEBUG
+//  debugcurrent(demo);
+//    function debugcurrent(json){      
+    var items = [];
+    jQuery.each(json.objects, function(ind, gm){
+      var gameText = '';
+      gameText += '<div class="LVgame">';
+      gameText += '<div class="LVlocation">'+(gm.game_site ? gm.game_site.event_site.name+' '+gm.game_site.name : 'Field TBD')+'</div>';
+      gameText += '<div class="LVteam1"><a href="'+gm.team_1.leaguevine_url+'">'+gm.team_1.name+'</a></div>';
+      gameText += '<div class="LVscore">';
+      if (gm.team_1_score > gm.team_2_score) {
+        gameText += '<span class="leader">'+gm.team_1_score+'</span> - <span class="trailer">'+gm.team_2_score+'</span>';
+      } else if (gm.team_1_score < gm.team_2_score) {
+        gameText += '<span class="trailer">'+gm.team_1_score+'</span> - <span class="leader">'+gm.team_2_score+'</span>';
+      } else {
+        gameText += '<span>'+gm.team_1_score+'</span> - <span>'+gm.team_2_score+'</span>';
+      };
+      gameText += '</div>';
+      gameText += '<div class="LVteam2"><a href="'+gm.team_2.leaguevine_url+'">'+gm.team_2.name+'</a></div>';
+      gameText += '</div>'
+      items.push(gameText);
+    });
+    var consolation_string = ['There are no current games.'];
+    var append_me = jQuery('<div/>', (items.length > 0 ? {'class':'LV-response', html: items.join('')} : {html: '<p>'+consolation_string[0]+'</p>'}));
+    append_me.appendTo($LV.children().filter('#LV-current'));  
     }
   );
-  // Format:
-  //games/?season_id=20368&starts_after=2013-09-10T23%3A03%3A03.002Z&access_token=efa78d2a3e
   //
+  // ***********************
   // Get upcoming games schedule.
+  // ***********************
   jQuery.getJSON(LV_api_url+'games/?season_id=20368'+
     '&starts_after='+now.toISOString()+
     '&access_token='+LV_access_token,
@@ -69,7 +98,9 @@ jQuery(document).ready(function() {
         var append_me = jQuery('<div/>', (items.length > 0 ? {'class':'LV-response', html: items.join('')} : {html: '<p>'+consolation_string+'</p>'}));
         append_me.appendTo($LV.children().filter('#LV-schedule'));
     });
+  // ***********************
   // Get past results.
+  // ***********************
   jQuery.getJSON(LV_api_url+'teams/?season_id=20368'+
     '&access_token='+LV_access_token, 
     function(json){
@@ -106,29 +137,7 @@ jQuery(document).ready(function() {
       jQuery('<div/>', {'class':'LV-response', html: results_table}).appendTo($LV.children().filter('#LV-results'));
     }
   );
-  function renderGames(json,currentBool,destination_selector){
-    var items = [];
-    jQuery.each(json.objects, function(ind, val){
-      var startdate = new Date(val.start_time);
-      var gameText = '';
-      gameText += '<div class="LVgame">';
-      gameText += '<a href="'+val.team_1.leaguevine_url+'">'+val.team_1.name+'</a>';
-      if (currentBool==0) {
-        gameText += ' vs ';
-      } else {
-        gameText += ' '+val.team_1_score+' - '+val.team_2_score+' ';
-      };
-      gameText += '<a href="'+val.team_2.leaguevine_url+'">'+val.team_2.name+'</a>';
-      gameText += '<p>';
-      gameText += monthString(startdate.getMonth(),1)+' '+startdate.getDate()+' at '+startdate.getHours()+':'+startdate.getMinutes();
-      gameText += (val.game_site ? ' on '+val.game_site : '');
-      gameText += '</div>';
-      items.push(gameText);
-    });
-    var consolation_string = ['There are no upcoming games.','There are no current games.'];
-    var append_me = jQuery('<div/>', (items.length > 0 ? {'class':'LV-response', html: items.join('')} : {html: '<p>'+consolation_string[currentBool]+'</p>'}));
-    append_me.appendTo($LV.children().filter(destination_selector));
-  };
+  
 });
 
 function monthString(ind, abbrvBool) {
